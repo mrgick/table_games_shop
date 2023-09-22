8c9a3dd19a459d29a6c89f3da9dde599b9781102
mrgick <mrgickcool@gmail.com>
2023-09-22 14:59:31 +0300

    add new apps: news and orders

--- src/settings/urls.py
+++ src/settings/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("main.urls")),
+    path("news/", include("news.urls")),
+    path("orders/", include("orders.urls")),
     path("admin/", admin.site.urls),
 ]
