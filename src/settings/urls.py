0d8a623081b1f172b6dcbf98d22409c751fa2807
mrgick <mrgickcool@gmail.com>
2023-09-22 14:55:43 +0300

    rename folder base to main

--- src/settings/urls.py
+++ src/settings/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("base.urls")),
+    path("", include("main.urls")),
     path("admin/", admin.site.urls),
 ]
