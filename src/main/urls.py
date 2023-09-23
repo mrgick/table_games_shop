09656e1eb5ec3befc7a7fad969563909dd47e5e0
mrgick <mrgickcool@gmail.com>
2023-09-23 15:05:00 +0300

    added authentication and registration.

--- src/main/urls.py
+++ src/main/urls.py
@@ -1,7 +1,25 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 urlpatterns = [
-    path("", views.Home.as_view()),
+    path("", views.Home.as_view(), name="home"),
     path("contact/", views.Contact.as_view(), name="contact"),
+    path("registration/", views.Registration.as_view(), name="registration"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path(
+        "login/",
+        LoginView.as_view(
+            authentication_form=AuthenticationForm,
+            template_name="pages/main/form.html",
+            extra_context={
+                "title": "Авторизация",
+                "action": ".",
+                "link": {"name": "Ещё нет аккаунта?", "value": "registration"},
+                "button": "Войти",
+            },
+        ),
+        name="login",
+    ),
 ]
--- src/main/views.py
+++ src/main/views.py
@@ -1,10 +1,41 @@
+from django.utils import timezone
+
 from django.views.generic.base import View, TemplateView
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls.base import reverse
+
 from django.http.response import JsonResponse
 
 class Home(TemplateView):
     """ Class for home page. """
-    template_name = "pages/index.html"
+    template_name = "pages/main/index.html"
 
 class Contact(TemplateView):
     """ Class for contacts page. """
-    template_name = "pages/contact.html"
+    template_name = "pages/main/contact.html"
+
+class Registration(FormView):
+    """ Class for the registration. """
+    template_name = "pages/main/form.html"
+    form_class = UserCreationForm
+    success_url = "/"
+
+    def form_valid(self, form):
+
+        user = form.save(commit=False)
+        user.is_staff = False
+        user.is_active = True
+        user.is_superuser = False
+        user.date_joined = timezone.now()
+        user.last_login = timezone.now()
+        user.save()
+
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        kwargs["title"] = "Регистрация"
+        kwargs["action"] = "."
+        kwargs["button"] = "Зарегистрироваться"
+        kwargs["link"] = {"name": "Уже есть аккаунт?", "value": "login"}
+        return super().get_context_data(**kwargs)
