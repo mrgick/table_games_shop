0e2e0beb04096597224573d599e20fca6d2c6eb5
mrgick <mrgickcool@gmail.com>
2023-10-06 13:53:08 +0300

    upd. Admin labels

--- src/news/apps.py
+++ src/news/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class NewsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "news"
-    # label = "Новости"
+    verbose_name = "Новости"
--- src/shop/apps.py
+++ src/shop/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class ShopConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "shop"
-    # label = "Магазин"
+    verbose_name = "Магазин"
